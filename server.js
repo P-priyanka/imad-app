95604568f880bb245ddefb8c9aef09c2a24528d1
P-priyanka <priyankasingh8568@gmail.com>
2017-09-01 17:58:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-two', function (req, res) {
-    res.send('Article two requested here');
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/article-three', function (req, res) {
-    res.send('Article three requested here');
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 app.get('/article-one', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
