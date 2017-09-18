2fe214a413ea5ca61a69c0850467687672c64bc5
P-priyanka <priyankasingh8568@gmail.com>
2017-09-18 23:18:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 //change the txt of main-text
 var element = document.getElementbyId('main-text');
 element.innerHTML='New Content';
+//Move the image
+var img = document.getElementbyId('pari');
+img.onclick = function() {
+    var interval = setInterval(moveRight,50);
+}
+    
