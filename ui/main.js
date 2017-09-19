1eb3e33ac2b49672f69e20263b71e047e3ba98e7
P-priyanka <priyankasingh8568@gmail.com>
2017-09-19 16:19:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,12 +8,6 @@ element.innerHTML='New Value';
 
 //Move the image
 var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft + 'px';
-}
 img.onclick = function() {
-    var interval = setInterval(moveRight,50);
+    img.style.marginLeft = '100px';
 };
-    
