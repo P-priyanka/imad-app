2394887e15785a70312b68fe9d765acf64f682b9
P-priyanka <priyankasingh8568@gmail.com>
2017-09-19 18:43:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,28 @@
 console.log('Loaded!');
-var counter = 0;
+
 //counter code
 
 var button = document.getElementById('counter');
 button.onclick = function() {
+    
+    //Create a Request object
+    
+    var request = new XMLHttpRequest();
+    
     //capture and restore it in a variable
-    
-    //render the variable in the correct span
-    counter = counter + 1;
-    
-    var span = document.getElementById('count');
-    span.innerHTML= counter.toString();
+    request.onreadystatechange = function() {
+    if(request.readyState --- XMLhttpRequest.DONE) {
+        //take some action
+        if (request.status == 200) {
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML= counter.toString();
+        }
+            
+      }
+        //Not yet Done
+    };
+    //Make a Request
+    request.open( 'GET', 'http://priyankasingh8568.imad.hasura-app.io' ,true);
+    request.send(null);
 };
