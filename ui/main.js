016ccfe519872e250b5efcb90bbd19b3bb575e21
P-priyanka <priyankasingh8568@gmail.com>
2017-09-19 17:55:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,13 @@
 console.log('Loaded!');
 
+//counter code
+var button = document.getElementById('counter');
+button.onclick = function() {
+    //capture and restore it in a variable
+    
+    //render the variable in the correct span
+    counter = counter+1;
+    
+    var span = document.getElementById('count');
+    span.innerHTML=counter.toString();
+};
