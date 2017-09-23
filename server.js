2b4cba3d9f996e96726c0150581861b587a5d728
P-priyanka <priyankasingh8568@gmail.com>
2017-09-23 22:23:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
+var names[];
 app.get('/submit-name/:name', function(req,res){
     //Get the name from request
     var name= req.param.name; //TODO
