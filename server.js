873e6e43773d03e7f6a143934f1d32a855879fb2
P-priyanka <priyankasingh8568@gmail.com>
2017-09-23 22:39:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,8 +99,7 @@ app.get ('/counter', function(req,res) {
 var names = [];
 app.get('/submit-name', function(req,res) { //URL: /submit-name?name=xxxx
     //Get the name from request
-    var name= req.query.name; //TODO
-    
+    var name = req.query.name; //TODO
     names.push(name);
     //JSON: Javascript Object Noatation
     res.send(JSON.stringify(names));
